18d1d9b1a4d130f680717e56b73a5d0adfc66ec0
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2015-01-20 22:13:32 +0000

    token key and conversation_id work

--- reader.rb
+++ reader.rb
@@ -1,11 +1,13 @@
 require 'open-uri'
+require 'rest-client'
 require 'JSON'
 
 # set API token and URL
 token = "$2y$10$Zx.LEihxwRd25tKwTsR1weBs3naVcP0xEhOOk9tIZSXjqBJgiZoq6"
 conversation_id = "c-8fca2030-7edd-11e4-a790-5b29cb36bdd0"
-polling_url = "https://api.usekimono.com/api/1/conversation/:conversation_id/read"
+# polling_url = "https://api.usekimono.com/api/1/conversation/:conversation_id/read"
 polling_url = "https://api.usekimono.com/api/1/conversation/list"
+
 
 # Specify request parameters
 params = {
@@ -16,6 +18,7 @@ params = {
 # puts token
 # puts conversation_id
 # puts polling_url
+
 # Prepare API request
 uri = URI.parse(polling_url)
 uri.query = URI.encode_www_form(params)
@@ -25,3 +28,5 @@ result = JSON.parse(open(uri).read)
 
 # Display results to screen
 puts JSON.pretty_generate result
+
+# data = JSON.load result
