2ce5713deb41e3ff34915ae86532f5a6be35c1c3
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2015-01-27 14:08:43 +0000

    can read all conversations

---
+++ README.md
@@ -0,0 +1,28 @@
+# Developing methods to use the [Kimono] API
+
+[Kimono]
+
+## How to run it
+```sh
+git clone git@github.com:StephanMusgrave/CraigsList_Scraper.git
+cd craigslist_scraper
+rails s
+
+```
+
+## Technologies used
+
+|Technology                 |Used for                        |
+|---------------------------|--------------------------------|
+|Kimono API                 |API for Kimono's enterprise messaging app|
+|Ruby 2.1.2                 |Main programming language       |
+
+
+## Developed by
+
+[Steve Musgrave]
+
+## Further work to do
+
+[Steve Musgrave]:https://github.com/StephanMusgrave
+[Kimono]:https://beta.usekimono.com
--- reader.rb
+++ reader.rb
@@ -2,9 +2,10 @@ require 'open-uri'
 require 'rest-client'
 require 'json'
 require "vine"
+require_relative 'secrets.rb'
 
 # set API token and URL
-token = "$2y$10$Zx.LEihxwRd25tKwTsR1weBs3naVcP0xEhOOk9tIZSXjqBJgiZoq6"
+token = API_TOKEN
 conversation_id = "c-8fca2030-7edd-11e4-a790-5b29cb36bdd0"
 # polling_url = "https://api.usekimono.com/api/1/conversation/:conversation_id/read"
 polling_url = "https://api.usekimono.com/api/1/conversation/list"
@@ -25,26 +26,31 @@ uri.query = URI.encode_www_form(params)
 result = JSON.parse(open(uri).read)
 
 # Display results to screen
-# puts JSON.pretty_generate result
-# puts result
-# data = JSON.load result
-# puts data
-
-result["conversations"].each do |conversation|
-  puts "conversation_id: #{conversation["conversation"]["id"]}"
-  puts "subject:         #{conversation["conversation"]["subject"]}"
-  puts "state:           #{conversation["conversation"]["state"]}"
-  puts "messenger_list:  #{conversation["conversation"]["messenger_list"]}"
-  # puts "user:            #{conversation["recipient"][0]["full_name"]}"
-
-  conversation["recipients"].each do |recipient|
-  # puts "...."
-    # puts "participant: #{recipient}"
-    puts "participant:  #{recipient[0]}" if recipient[0]
-    # puts "group: #{recipient[1]}" if recipient[1] 
-    # puts "bot:   #{recipient[2]}" if recipient[2] 
-  end
-  puts '------------------------'
-  puts '------------------------'
-
+def show_JSON(result)
+  puts JSON.pretty_generate result
 end
+
+def display_all(result,state)
+  result["conversations"].each do |conversation|
+    if conversation["conversation"]["state"] == state
+      puts "conversation_id:  #{conversation["conversation"]["id"]}"
+      puts "subject:          #{conversation["conversation"]["subject"]}"
+      puts "state:            #{conversation["conversation"]["state"]}"
+      puts "recipients user:  #{conversation["recipients"]["user"][0]["full_name"]}" if conversation["recipients"]["user"] != []
+      puts "recipients group: #{conversation["recipients"]["group"][0]["name"]}" if conversation["recipients"]["group"] != []
+      puts "recipients bot:   #{conversation["recipients"]["bot"][0]["name"]}" if conversation["recipients"]["bot"] != []
+      puts "messenger_list:   #{conversation["conversation"]["messenger_list"]}"
+      i = 0
+      conversation["messages"].each do |message|
+        puts "  #{i}.  #{conversation["messages"][i]["user"]["full_name"]}: #{conversation["messages"][i]["message"]}"
+        i += 1
+        # puts "...."
+      end
+    end
+    puts '------------------------'
+  end
+end
+
+# puts API_TOKEN
+display_all(result,"active")
+# show_JSON(result)
